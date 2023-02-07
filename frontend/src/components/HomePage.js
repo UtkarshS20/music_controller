181f1f7c6743270abed4d43bded9626e35f23fbc
Utkarsh <utkarsh2001saxena@gmail.com>
2023-02-07 10:50:35 +0530

    Day 9 commit 1

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
+import { Button, Grid, Typography, ButtonGroup } from "@mui/material";
 import {
   BrowserRouter as Router,
   Switch,
@@ -14,14 +15,39 @@ export default class HomePage extends Component {
   constructor(props) {
     super(props);
   }
+
+  renderHomePage() {
+    return <Grid container spacing={3}>
+      <Grid item xs={12} align = "center">
+        <Typography variant="h3" compact="h3">
+          House Party
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align = "center">
+        <ButtonGroup disableElevation variant="contained" color="primary">
+          <Button color="primary" to='/join' component={Link}>
+            Join a Room
+          </Button>
+          <Button color="secondary" to='/create' component={Link}>
+            Create a Room
+          </Button>
+        </ButtonGroup>
+      </Grid>
+    </Grid>;
+  }
+
   render() {
-    return (<Router>
+    return (
+      <Router>
         <Switch>
-            <Route exact path='/'><p>This is home page</p></Route>
-            <Route path='/join' component={RoomJoinPage} />
-            <Route path='/create' component={CreateRoomPage} />
-            <Route path='/room/:roomCode' component={Room} />
+          <Route exact path="/">
+            { this.renderHomePage()}
+          </Route>
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create" component={CreateRoomPage} />
+          <Route path="/room/:roomCode" component={Room} />
         </Switch>
-    </Router>);
+      </Router>
+    );
   }
 }
